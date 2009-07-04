cb8bb50040dfcf1c85e7537dd275de2f5e7228d4
Jeff Dean <jeff@zilkey.com>
2009-07-04 02:22:00 -0400

    Made sure test coverage for ancestors was correct

--- spec/active_api/element/collection_spec.rb
+++ spec/active_api/element/collection_spec.rb
@@ -24,14 +24,15 @@ module ActiveApi
         end
       end
 
-      @article = Article.new
-      @article.id = 1
-      @article.title = "Some title"
-      user = User.new
-      comment = Comment.new
-      comment.article = @article
-      comment.user = user
-      @article.comments = [comment]
+      @article1 = Article.new :title => Faker::Company.bs
+      @article2 = Article.new :title => Faker::Company.bs
+
+      @user = User.new
+      @comment1 = Comment.new :article => @article1, :user => @user
+      @comment2 = Comment.new :article => @article2, :user => @user
+
+      @article1.comments = [@comment1]
+      @article2.comments = [@comment2]
     end
 
     after do
@@ -40,22 +41,24 @@ module ActiveApi
 
     describe "with an array" do
       it "works" do
-        element = Element::Collection.new [@article], :node => :article
+        element = Element::Collection.new [@article1], :node => :article
         doc = element.build_xml.doc
-        doc.xpath("/articles/article/title").first.inner_text.should == @article.title
+        doc.xpath("/articles/article/title").first.inner_text.should == @article1.title
       end
     end
 
     it "stores a reference to the parents" do
-      element = Element::Collection.new [@article], :node => :article
+      element = Element::Collection.new [@article1], :node => :article
       doc = element.build_xml.doc
-      doc.xpath("/articles/article/comments/comment/title").first.inner_text.should == @article.title
+      doc.xpath("/articles/article/comments/comment/title").first.inner_text.should == @article1.title
     end
 
     it "stores a reference to the node ancestors" do
-      element = Element::Collection.new [@article], :node => :article
+      element = Element::Collection.new [@article1, @article2], :node => :article
       doc = element.build_xml.doc
-      doc.xpath("/articles/article/comments/comment/user/title").first.inner_text.should == @article.title
+      doc.xpath("/articles/article").length.should == 2
+      doc.xpath("/articles/article/comments/comment/user/title").first.inner_text.should == @article1.title
+      doc.xpath("/articles/article/comments/comment/user/title").last.inner_text.should == @article2.title
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,19 +10,46 @@ Spec::Runner.configure do |config|
   config.mock_with :rr
 end
 
-class Author
-  attr_accessor :id, :name
+class ObjectHelper
+  class << self
+    def fields(*args)
+      args.each do |field|
+        field(field)
+      end
+    end
+
+    private
+
+    def field(field_name)
+      define_method field_name do
+        attributes[field_name]
+      end
+      define_method "#{field_name}=" do |value|
+        attributes[field_name] = value
+      end
+    end
+  end
+
+  attr_reader :attributes
+  def initialize(options = {})
+    @attributes = options
+  end
+
 end
 
-class Article
-  attr_accessor :id, :author_id, :title, :published_on, :comments
+class Author < ObjectHelper
+  fields :id, :name
 end
 
-class User
-  attr_accessor :id, :username
+class Article < ObjectHelper
+  fields :id, :author, :title, :published_on, :comments
 end
 
-class Comment
-  attr_accessor :id, :article, :article, :user, :text
+class User < ObjectHelper
+  fields :id, :username
+end
+
+class Comment < ObjectHelper
+  fields :id, :article, :user, :text
 end
 
